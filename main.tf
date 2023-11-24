ad87b746e36a1088cd9ea81a0a86bb1a949a85e5
Simran Kaur <sammy@simrans-mbp.home>
2023-11-24 15:38:59 -0500

    Updated EKS config

--- main.tf
+++ main.tf
@@ -1,49 +1,46 @@
-module "eks_eks-managed-node-group" {
-  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
-  version = "19.20.0"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
-  name            = "managed-node-group"
   cluster_name    = "test-cluster"
-  cluster_version = "1.28"
+  cluster_version = "1.27"
 
-  subnet_ids = ["subnet-0016ad2c1d76e8fe7", "subnet-0de774a5b67c4ec28", "subnet-0bddfbcec7ad1ea99"]
+  cluster_endpoint_public_access  = true
 
-  // The following variables are necessary if you decide to use the module outside of the parent EKS module context.
-  // Without it, the security groups of the nodes are empty and thus won't join the cluster.
-  #cluster_primary_security_group_id = ""
-  #vpc_security_group_ids            = [module.eks.node_security_group_id]
-
-  // Note: `disk_size`, and `remote_access` can only be set when using the EKS managed node group default launch template
-  // This module defaults to providing a custom launch template to allow for custom security groups, tag propagation, etc.
-  // use_custom_launch_template = false
-  // disk_size = 50
-  //
-  //  # Remote access cannot be specified with a launch template
-  //  remote_access = {
-  //    ec2_ssh_key               = module.key_pair.key_pair_name
-  //    source_security_group_ids = [aws_security_group.remote_access.id]
-  //  }
-
-  min_size     = 1
-  max_size     = 2
-  desired_size = 1
-
-  instance_types = ["t2.micro"]
-  capacity_type  = "SPOT"
-
-  labels = {
-    Environment = "test"
-    GithubRepo  = "terraform-aws-eks"
-    GithubOrg   = "terraform-aws-modules"
-  }
-
-  taints = {
-    dedicated = {
-      key    = "dedicated"
-      value  = "gpuGroup"
-      effect = "NO_SCHEDULE"
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
     }
   }
+
+  vpc_id                   = "vpc-08e82dceb34c33b38"
+  subnet_ids               = ["subnet-0016ad2c1d76e8fe7", "subnet-0de774a5b67c4ec28", "subnet-0bddfbcec7ad1ea99"]
+  control_plane_subnet_ids = ["subnet-0016ad2c1d76e8fe7", "subnet-0de774a5b67c4ec28", "subnet-0bddfbcec7ad1ea99"]
+
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
 
   tags = {
     Environment = "dev"
