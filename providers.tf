1d634e072c1410dd4458b8729c6d816b4f7ea386
Simran Kaur <sammy@simrans-mbp.home>
2023-11-24 14:45:29 -0500

    Updated

--- providers.tf
+++ providers.tf
@@ -7,3 +7,17 @@ terraform {
     }
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.26.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+}
